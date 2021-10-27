990f26faebe467ad7f5c93d1a75400c26b764007
rubenbalayan <ruben.rm@mail.ru>
2021-10-27 17:40:08 +0400

    Pre last commit

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,14 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../Styles/Login.css";
+import { useHistory } from "react-router-dom";
+import axios from 'axios';
 
-class Login extends React.Component{
+const Login = () => {
+
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+    //const [token,setToken] = useState();
+    const [data,setData] = useState();
+
+    const history = useHistory();
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+      }
+      
+    const handlePasswordChange = (event) => { 
+        setPassword(event.target.value);
+      }
+
+    const handleSubmit = async (event) =>
+    {
+       //Fetching a call to ensure that credentials are correct
+
+       let data = await loginUser({username,password});
+
+       debugger 
+        
+        //history.push('/userui');   
+       
+    }
+
+    async function loginUser(credentials)
+    {
+        return fetch("http://192.168.88.92:8085/api/auth/login",
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': credentials.length
+              }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            setData(data);
+        })
+        .catch((err) => console.log(err));
+    }
+    
+
+    return(
+
+        <div id="loginMain">
+                <form id="loginForm" onSubmit={handleSubmit} >
+                    <label for = "username">Username</label>
+                    <input type="text" placeholder="Enter Username" name="username" value={username} onChange={handleUsernameChange} required></input>
+                    
+                    <label for = "password">Password</label>
+                    <input type="password" placeholder="Enter Password" name="password" value={password} onChange={handlePasswordChange} required></input>
+
+                    <button type="submit">Submit</button>
+                </form>
+
+        </div>
+
+    );
+
+}
+
+
+/*class Login extends React.Component{
     constructor(props)
     {
         super(props);
         
         this.state = {
             username: "",
-            password: ""
+            password: "",
+            data : []
         };
 
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
@@ -28,6 +98,17 @@ class Login extends React.Component{
     handleSubmit(event)
     {
        //Fetching a call to ensure that credentials are correct
+        let username = this.state.username;
+        let password = this.state.password;
+        let body = {username, password};
+        fetch("http://192.168.88.92:8085/api/auth/login", {
+        method: "POST",
+        headers: {"content-Type": "application/json"},
+        body: JSON.stringify(body)
+        }).then(results => results.json()).then(data => this.setState({ data: data }));
+
+        alert(this.state.data);
+
        this.props.history.push('/userui');
        
     }
@@ -49,6 +130,6 @@ class Login extends React.Component{
             </div>
         );
     }
-}
+}*/
 
 export default Login;
